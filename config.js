7a626baffe825c0cbb485a71a55ba3c70bdd92b2
bix12as <nxfelibata.biz+sds@gmail.com>
2025-01-05 15:34:15 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,193 @@
+const config = {
+    whatsappPhoneNumber: "+27717311486",
+    platforms: [
+        {
+            name: "Instagram",
+            value: "Instagram"
+        },
+        {
+            name: "TikTok",
+            value: "TikTok"
+        },
+        {
+            name: "Facebook",
+            value: "Facebook"
+        },
+        {
+            name: "Twitter",
+            value: "Twitter"
+        },
+        {
+            name: "YouTube",
+            value: "YouTube"
+        },
+        {
+            name: "Snapchat",
+            value: "Snapchat"
+        },
+        {
+            name: "LinkedIn",
+            value: "LinkedIn"
+        },
+        {
+            name: "Pinterest",
+            value: "Pinterest"
+        },
+        {
+            name: "Reddit",
+            value: "Reddit"
+        }
+    ],
+    services: [
+        {
+            platform: "Instagram",
+            plans: [
+                {
+                    name: "Instagram Likes",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "Instagram Followers",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        },
+        {
+            platform: "TikTok",
+            plans: [
+                {
+                    name: "TikTok Views [bot] 3k - Free limited",
+                    description: "Boost your TikTok views. Order List",
+                    price: "R0"
+                },
+                {
+                    name: "TikTok Likes",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "TikTok Shares",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "TikTok Saves",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "TikTok Followers",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        },
+        {
+            platform: "Facebook",
+            plans: [
+                {
+                    name: "Facebook Likes",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "Facebook Shares",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        },
+        {
+            platform: "Twitter",
+            plans: [
+                {
+                    name: "Twitter Followers",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "Twitter Retweets",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        },
+        {
+            platform: "YouTube",
+            plans: [
+                {
+                    name: "YouTube Views",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "YouTube Subscribers",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        },
+        {
+            platform: "Snapchat",
+            plans: [
+                {
+                    name: "Snapchat Views",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "Snapchat Followers",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        },
+        {
+            platform: "LinkedIn",
+            plans: [
+                {
+                    name: "LinkedIn Connections",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "LinkedIn Shares",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        },
+        {
+            platform: "Pinterest",
+            plans: [
+                {
+                    name: "Pinterest Saves",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "Pinterest Followers",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        },
+        {
+            platform: "Reddit",
+            plans: [
+                {
+                    name: "Reddit Upvotes",
+                    description: "Contact For Quote",
+                    price: "R0"
+                },
+                {
+                    name: "Reddit Followers",
+                    description: "Contact For Quote",
+                    price: "R0"
+                }
+            ]
+        }
+    ]
+};
